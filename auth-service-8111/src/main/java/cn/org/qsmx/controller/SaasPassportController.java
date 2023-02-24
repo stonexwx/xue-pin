20f81fc9284637e12e73ae4c03c26e260aafa220
xwx <1162027477@qq.com>
2023-02-24 10:22:25 +0800

    扫码登录完成

--- auth-service-8111/src/main/java/cn/org/qsmx/controller/SaasPassportController.java
+++ auth-service-8111/src/main/java/cn/org/qsmx/controller/SaasPassportController.java
@@ -3,6 +3,7 @@ package cn.org.qsmx.controller;
 import cn.org.qsmx.base.BaseInfoProperties;
 import cn.org.qsmx.pojo.Users;
 import cn.org.qsmx.pojo.bo.RegistLoginBO;
+import cn.org.qsmx.pojo.vo.SaasUserVO;
 import cn.org.qsmx.pojo.vo.UserVO;
 import cn.org.qsmx.result.GraceJSONResult;
 import cn.org.qsmx.result.ResponseStatusEnum;
@@ -12,6 +13,7 @@ import cn.org.qsmx.util.JWTUtil;
 import cn.org.qsmx.util.SMSUtils;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.User;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -28,6 +32,9 @@ public class SaasPassportController extends BaseInfoProperties {
 
     @Autowired
     private JWTUtil jwtUtil;
+
+    @Autowired
+    private UsersService usersService;
 
     /**
      * 生成二维码登录token
@@ -46,6 +53,12 @@ public class SaasPassportController extends BaseInfoProperties {
         return GraceJSONResult.ok(qrToken);
     }
 
+    /**
+     * 手机端使用HR角色进行扫码操作
+     * @param qrToken
+     * @param request
+     * @return
+     */
     @PostMapping("scanCode")
     public GraceJSONResult scanCode(String qrToken,HttpServletRequest request){
         if(StringUtils.isEmpty(qrToken)){
@@ -74,7 +87,100 @@ public class SaasPassportController extends BaseInfoProperties {
         redis.set(SAAS_PLATFORM_LOGIN_TOKEN+":"+qrToken,preToken,5*60);
 
         //redis写入标记，当前qeToken失效，
-        redis.set(SAAS_PLATFORM_LOGIN_TOKEN_READ+":"+qrToken,"1"+preToken,5*60);
+        redis.set(SAAS_PLATFORM_LOGIN_TOKEN_READ+":"+qrToken,"1,"+preToken,5*60);
         return GraceJSONResult.ok(preToken);
     }
+
+    /**
+     * 前端每隔一段时间定时查询qeToken是否被读取，用于页面的展示标记判断
+     * @param qrToken
+     * @return
+     */
+    @PostMapping("codeHasBeenRead")
+    public GraceJSONResult codeHasBeenRead(String qrToken){
+        String readStr = redis.get(SAAS_PLATFORM_LOGIN_TOKEN_READ+":"+qrToken);
+        List list = new ArrayList();
+
+        if(StringUtils.isNotBlank(readStr)){
+            String[] readStrArray = readStr.split(",");
+            if(readStrArray.length>=2){
+                list.add(Integer.valueOf(readStrArray[0]));
+                list.add(readStrArray[1]);
+            }else {
+                list.add(0);
+            }
+            return GraceJSONResult.ok(list);
+        }else {
+            return GraceJSONResult.ok(list);
+        }
+    }
+
+    /**
+     * 手机端点击登录，携带preToken与后端进行判断，校验成功就能登录
+     * @param userId
+     * @param qrToken
+     * @param preToken
+     * @return
+     */
+    @PostMapping("goQRLogin")
+    public GraceJSONResult goQRLogin(String userId,String qrToken,String preToken){
+        String preTokenRedisArr = redis.get(SAAS_PLATFORM_LOGIN_TOKEN_READ+":"+qrToken);
+
+        if (StringUtils.isNotBlank(preTokenRedisArr)){
+            String preTokenRedis = preTokenRedisArr.split(",")[1];
+            if (preTokenRedis.equalsIgnoreCase(preToken)){
+                Users hrUser = usersService.getById(userId);
+                if(hrUser == null){
+                    return  GraceJSONResult.errorCustom(ResponseStatusEnum.USER_NOT_EXIST_ERROR);
+                }
+
+                //存入用户信息到redis,因为H5在未登陆的情况下，拿不到用户id
+                redis.set(REDIS_SAAS_USER_INFO+":temp"+preToken,new Gson().toJson(hrUser),5*60);
+
+            }
+        }
+        return GraceJSONResult.ok();
+    }
+
+    /**
+     * 登录页面跳转
+     * @param preToken
+     * @return
+     */
+    @PostMapping("CheckLogin")
+    public GraceJSONResult CheckLogin(String preToken){
+
+        if (StringUtils.isBlank(preToken)){
+            return GraceJSONResult.error();
+        }
+
+        String userJson = redis.get(REDIS_SAAS_USER_INFO+":temp"+preToken);
+
+        if (StringUtils.isBlank(userJson)){
+            return GraceJSONResult.errorCustom(ResponseStatusEnum.USER_NOT_EXIST_ERROR);
+        }
+
+        String saasUserToken = jwtUtil.createJWTPrefix(userJson,TOKEN_SAAS_PREFIX);
+        redis.set(REDIS_SAAS_USER_INFO+":"+saasUserToken,userJson);
+
+        return GraceJSONResult.ok(saasUserToken);
+    }
+
+    @GetMapping("info")
+    public GraceJSONResult info(String token){
+        String userJson = redis.get(REDIS_SAAS_USER_INFO+":"+token);
+
+        Users users = new Gson().fromJson(userJson,Users.class);
+
+        SaasUserVO saasUserVO = new SaasUserVO();
+        BeanUtils.copyProperties(users,saasUserVO);
+
+        return GraceJSONResult.ok(saasUserVO);
+    }
+
+    @PostMapping("logout")
+    public GraceJSONResult logout(String token){
+        return GraceJSONResult.ok();
+    }
+
 }
---
+++ hire-pojo/src/main/java/cn/org/qsmx/pojo/vo/SaasUserVO.java
@@ -0,0 +1,16 @@
+package cn.org.qsmx.pojo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class SaasUserVO {
+    private String username;
+    private String name;
+    private String face;
+}
